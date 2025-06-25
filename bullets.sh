777eec98aec89400c10c7c9ba4b814c64f1426b4
Pegasus <122263374+Pegasus0xx@users.noreply.github.com>
2025-06-25 16:58:58 +0300

    Update URL extraction filter: excluded common service domains (e.g. google, github, adobe)

--- bullets.sh
+++ bullets.sh
@@ -29,12 +29,47 @@ echo -e "${YELLOW}[*] Decoding file: $APK_FILE into folder: $APK_FOLDER${NC}"
 apktool d "$APK_FILE" -o "$APK_FOLDER" &>/dev/null
 
 echo -e "${GREEN}[+] Scanning folder: $APK_FOLDER with nuclei${NC}"
-echo "$APK_FOLDER" | nuclei -t "$NUCLEI_TEMPLATE_PATH"
+echo "$APK_FOLDER" | nuclei -t "$NUCLEI_TEMPLATE_PATH" | \
+grep -Pv "(w3|android|github|schemas\.android|google|goo\.gl|apache\.org|ns\.adobe\.com|schemas\.microsoft\.com|developer\.apple\.com|apple\.com|default\.url)"
 
 echo -e "${YELLOW}[*] Extracting HTTP Auth credentials from smali files...${NC}"
 
 cd "$APK_FOLDER" || { echo -e "${RED}Failed to enter directory $APK_FOLDER${NC}"; exit 1; }
 
-bash -c 'grep -E -r --include=\*.smali "const-string|invoke-virtual.*HttpAuthHandler;->proceed" . | awk '\''function is_cred(s) { return (length(s) >= 3 && length(s) <= 32) && (s ~ /^[a-zA-Z0-9@._-]+$/) } { if ($0 ~ /const-string/) { match($0, /const-string (p[0-9]+), "([^"]+)"/, arr); vars[arr[1]] = arr[2] } if ($0 ~ /invoke-virtual.*HttpAuthHandler;->proceed/) { match($0, /invoke-virtual \{([^}]+)\}/, regs); split(regs[1], regs_arr, ", "); user = vars[regs_arr[2]]; pass = vars[regs_arr[3]]; if (is_cred(user) && is_cred(pass)) { print "Username: " user " | Password: " pass } } }'\'''
+bash -c 'grep -E -r --include=\*.smali "const-string|invoke-virtual.*HttpAuthHandler;->proceed" . | awk '\''
+  function is_cred(s) {
+    if (s !~ /^[a-zA-Z0-9@._-]+$/) return 0;
+
+    count = 0;
+    for (i=2; i<=length(s); i++) {
+      c = substr(s, i, 1);
+      if (c ~ /[A-Z]/) count++;
+    }
+    if (count >= 3) return 0;
+
+    for (i=1; i<length(s); i++) {
+      c1 = substr(s, i, 1);
+      c2 = substr(s, i+1, 1);
+      if (c1 ~ /[A-Z]/ && c2 ~ /[a-z]/) return 0;
+    }
+
+    return 1;
+  }
+  {
+    if ($0 ~ /const-string/) {
+      match($0, /const-string (p[0-9]+), "([^"]+)"/, arr);
+      vars[arr[1]] = arr[2];
+    }
+    if ($0 ~ /invoke-virtual.*HttpAuthHandler;->proceed/) {
+      match($0, /invoke-virtual \{([^}]+)\}/, regs);
+      split(regs[1], regs_arr, ", ");
+      user = vars[regs_arr[2]];
+      pass = vars[regs_arr[3]];
+      if (is_cred(user) && is_cred(pass)) {
+        print "Username: " user " | Password: " pass;
+      }
+    }
+  }
+'\'''
 
 echo -e "${GREEN}[*] Done extracting credentials.${NC}"
